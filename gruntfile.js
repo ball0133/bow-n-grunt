c0bee17462d6081300eab7961c2144e9d207da19
KariBall <ball0>
2015-12-03 17:42:44 -0500

    Create
    Bower & Grunt tasks

---
+++ gruntfile.js
@@ -0,0 +1,127 @@
+// Gruntfile.js
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        copy: {
+            main: {
+                files: [
+                // flattens results to a single level
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/font-awesome/fonts/*'],
+                        dest: 'dist/fonts',
+                        filter: 'isFile'
+                },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/bootstrap/dist/css/bootstrap.css'],
+                        dest: 'dist/css/',
+                        filter: 'isFile'
+                },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/bootstrap/dist/js/bootstrap.js'],
+                        dest: 'dist/js/',
+                        filter: 'isFile'
+                },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/font-awesome/css/font-awesome.css'],
+                        dest: 'dist/css/',
+                        filter: 'isFile'
+                },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/jquery/dist/jquery.js'],
+                        dest: 'dist/js/',
+                        filter: 'isFile'
+                },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/jquery.easing/js/*'],
+                        dest: 'dist/js/',
+                        filter: 'isFile'
+                },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['css/main.css'],
+                        dest: 'dist/css/',
+                        filter: 'isFile'
+                },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['js/main.js'],
+                        dest: 'dist/js/',
+                        filter: 'isFile'
+                },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['index.html'],
+                        dest: 'dist/',
+                        filter: 'isFile'
+                },
+                {
+                        expand: true,
+                        flatten: true,
+                        src: ['images/*'],
+                        dest: 'dist/images/',
+                        filter: 'isFile'
+                },
+                {
+                        expand: true,
+                        flatten: true,
+                        src: ['fonts/*'],
+                        dest: 'dist/fonts/',
+                        filter: 'isFile'
+                },
+
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            my_target: {
+                files: {
+                    'dist/js/main.min.js': ['dist/js/jquery.js', 'dist/js/jquery.easing.js', 'dist/js/jquery.easing.min.js', 'dist/js/jquery.easing.compatibility.js', 'dist/js/main.js', 'dist/js/bootstrap.js']
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'dist/css/main.min.css': ['dist/css/bootstrap.css', 'dist/css/font-awesome.css', 'dist/css/main.css']
+                }
+            }
+        },
+        // Deletes all .js files, but skips min.js files, same with css files
+        clean: {
+            css: ["dist/css/*.css", "!dist/css/*.min.css"],
+            js: ["dist/js/*.js", "!dist/js/*.min.js"]
+        }
+
+    });
+    // Load the plugins that provides the tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    //Calling 'grunt default' will run all tasks 
+    grunt.registerTask('default', ['copy', 'uglify', 'cssmin', 'clean']);
+}
